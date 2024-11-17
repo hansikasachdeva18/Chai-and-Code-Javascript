cf242efe6bf399adbad7addcabc57005de5819ad
Hansika Sachdeva <86574049+hansikasachdeva18@users.noreply.github.com>
2024-11-17 09:35:40 +0000

    committed

---
+++ 01_Basics/let_const_var.js
@@ -0,0 +1,21 @@
+const a="Hansika";
+let b="Naman";
+var c="Mamta";
+d="Mukesh";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+b="Naman Sachdeva";
+c="Mamta Sachdeva";
+d="Mukesh Sachdeva";
+
+console.table([a,b,c,d]);
+
+let check;
+var hey;
+
+console.log(check);
+console.log(hey);
