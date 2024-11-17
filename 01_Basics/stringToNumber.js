0d64ce50ae7301274f6f111fdd164518f1131aec
Hansika Sachdeva <86574049+hansikasachdeva18@users.noreply.github.com>
2024-11-17 18:56:06 +0000

    committed

---
+++ 01_Basics/stringToNumber.js
@@ -0,0 +1,29 @@
+let z=7;
+
+console.log(-z); // - 7
+
+let value=-z;
+console.log(value); // -7
+
+let valueOne= -9;
+console.log(-valueOne); // 9 
+
+let a=console.log("5"+1+1);
+let b=console.log(3+"1"+"1");
+let c=console.log(3+"1"+1);
+let d=console.log(3+1+"1");
+ 
+ 
+let hansika="5"+1+1;
+let naman=3+"1"+"1";
+let mamta=3+"1"+1;
+let mukesh=3+1+"1";
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+console.log(typeof hansika);
+console.log(typeof naman);
+console.log(typeof mamta);
+console.log(typeof mukesh);
