7a7d729261f9b098eae04308c3ff05ee38f201dd
Hansika Sachdeva <86574049+hansikasachdeva18@users.noreply.github.com>
2024-11-18 18:14:47 +0000

    committed

---
+++ 01_Basics/comparision.js
@@ -0,0 +1,15 @@
+console.log("01">0); // true
+console.log("2">1) // true
+
+
+// weird behaviour of js 
+
+console.log(null==0); // false  null zero nahin ha
+console.log(null >0 ); // false null 0 se greater nahin h 
+
+console.log( null < 0 ); // false null se badha 0 nahin h 
+console.log(null >= 0); // true par null >=0 hai weird
+
+// these operators work very differently in javascript so we have typescript for strict rules.
+
+
